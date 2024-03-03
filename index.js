1216fa6ba72b8d5c7852c22e4bc92ce44ebbdfef
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-03 09:24:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+    let rna = [];
+    for(let i = 0; i < dna.length; i++){
+      if(dna.charAt(i) === 'T'){
+        rna.push('U');
+      } else if (dna.charAt(i) === 'U'){
+        rna.push('T');
+      } else{
+      rna.push(dna.charAt(i));
+      }
+          
+     }
+    return rna.join('');
+  }
+
+
+  console.log(DNAtoRNA("GATTCCACCGACTTCCCAAGTACCGGAAGCGCGACCAACTCGCACAGC"));
