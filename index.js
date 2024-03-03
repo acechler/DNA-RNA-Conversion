7d3db74f06ad05e055eb37a46a9c448cd602adb4
Aaron Echler <44124054+acechler@users.noreply.github.com>
2024-03-03 09:25:20 -0500

    Add some documentation.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Simple node project to show DNA and RNA conversion.
+
 function DNAtoRNA(dna) {
     // create a function which returns an RNA sequence from the given DNA sequence
     let rna = [];
